85bc5edbbe7a3a22050f54c7301cc041cf12a6a1
Aron Roberts <aron@socrates.berkeley.edu>
2013-11-22 16:41:44 -0800

    CSPACE-6278: Added test build of the Services layer, reorganized 'require' layout.

--- manifests/init.pp
+++ manifests/init.pp
@@ -43,43 +43,59 @@
 # puppet apply --modulepath=/etc/puppet/modules ./manifests/init.pp
 
 class cspace_source {
-	
-	file { 'Create CollectionSpace source directory':
-	    path   => "/tmp/cspace-source",
-	    ensure => "directory",
-	}
     
-	vcsrepo { '/tmp/cspace-source/application':
-	    require => File['Create CollectionSpace source directory'],
+    file { 'Create CollectionSpace source directory':
+        path   => "/tmp/cspace-source",
+        ensure => "directory",
+    }
+    
+    # Download and build the Application layer
+    
+    vcsrepo { '/tmp/cspace-source/application':
+        require => File['Create CollectionSpace source directory'],
         # require  => [ Package["git"] ],
-	    provider => 'git',
+        ensure => latest,
+        provider => 'git',
         source   => 'https://github.com/collectionspace/application.git',
         revision => 'master',
     }
+    
+    exec { 'Maven clean of Application layer source':
+        require => Vcsrepo['/tmp/cspace-source/application'],
+        command => 'mvn clean',
+        cwd => '/tmp/cspace-source/application',
+        path => ['/bin', '/usr/bin'],
+    }
 
-	vcsrepo { '/tmp/cspace-source/services':
+    # Download and build the Services layer
+    
+    vcsrepo { '/tmp/cspace-source/services':
         require => File['Create CollectionSpace source directory'],
         # require  => [ Package["git"] ],
-	    provider => 'git',
+        ensure => latest,
+        provider => 'git',
         source   => 'https://github.com/collectionspace/services.git',
         revision => 'master',
     }
+    
+    exec { 'Maven clean of Services layer source':
+        require => Vcsrepo['/tmp/cspace-source/services'],
+        command => 'mvn clean',
+        cwd => '/tmp/cspace-source/services',
+        path => ['/bin', '/usr/bin'],
+    }
 
-	vcsrepo { '/tmp/cspace-source/ui':
+    # Download the UI layer
+
+    vcsrepo { '/tmp/cspace-source/ui':
         require => File['Create CollectionSpace source directory'],
         # require  => [ Package["git"] ],
-	    provider => 'git',
+        ensure => latest,
+        provider => 'git',
         source   => 'https://github.com/collectionspace/ui.git',
         revision => 'master',
     }
-	
-	# For testing
-	exec { 'Maven clean of Application layer source':
-        require => Vcsrepo['/tmp/cspace-source/application'],
-	    command => 'mvn clean',
-	    cwd => '/tmp/cspace-source/application',
-	    path => ['/bin', '/usr/bin'],
-	}
+
 }
 
 include cspace_source
