36d4047e4f53055bd4e72b86faf9f285d957b6cd
Aron Roberts <aron@socrates.berkeley.edu>
2013-12-02 18:42:20 -0800

    CSPACE-6278: More parameterization.

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,80 +42,80 @@
 # Test standlone with a reference to the modulepath in which that module is installed; e.g.
 # puppet apply --modulepath=/etc/puppet/modules ./manifests/init.pp
 
-class cspace_source( $env_vars ) {
+$mvn_clean_cmd = 'mvn clean'
+$mvn_clean_install_cmd = "${mvn_clean_cmd} install -DskipTests"
+
+class cspace_source( $env_vars, $exec_paths = [ '/bin', '/usr/bin' ] ) {
     
     file { 'Create CollectionSpace source directory':
-        path   => '/tmp/cspace-source',
         ensure => 'directory',
+        path   => '/tmp/cspace-source',
     }
     
     # Download and build the Application layer
     
     vcsrepo { '/tmp/cspace-source/application':
-        require  => File[ 'Create CollectionSpace source directory' ],
-        # require  => [ Package["git"] ],
         ensure   => latest,
         provider => 'git',
         source   => 'https://github.com/collectionspace/application.git',
         revision => 'master',
+        require  => File[ 'Create CollectionSpace source directory' ],
     }
     
-    exec { 'Maven clean of Application layer source':
-        require => Vcsrepo[ '/tmp/cspace-source/application' ],
-        command => 'mvn clean',
+    exec { 'Maven clean install of Application layer source':
+        command => $mvn_clean_install_cmd,
         cwd     => '/tmp/cspace-source/application',
-        path    => [ '/bin', '/usr/bin' ],
+        path    => $exec_paths,
+        require => Vcsrepo[ '/tmp/cspace-source/application' ],
     }
 
     # Download and build the Services layer
     
     vcsrepo { '/tmp/cspace-source/services':
-        require  => File[ 'Create CollectionSpace source directory' ],
-        # require  => [ Package["git"] ],
         ensure   => latest,
         provider => 'git',
         source   => 'https://github.com/collectionspace/services.git',
         revision => 'master',
+        require  => File[ 'Create CollectionSpace source directory' ],
     }
-	
-	# Experiment with setting environment variables on the fly.
-	#
-	# Note that the 'user' property is currently commented out.
-	# If we set that property, it appears that the 'exec':
-	#
-	# a) must be run as root; and
-	# b) is then given root's environment, not that of the designated user.
-	# (That may not always be what we intend.)
-	#
-	# When the 'user' property is set and this 'exec' is then run as
-	# a non-root user, the following error occurs:
-	# "Error: Parameter user failed on Exec[...]:
-	# Only root can execute commands as other users""
-	
-	exec { 'Check values of environment variables':
-	    command   => 'env',
-	    path      => [ '/bin', '/usr/bin' ],
-	    logoutput => 'true',
-		environment => [ $env_vars ]
-	    # user      => 'cspace',
+    
+    # Experiment with setting environment variables on the fly.
+    #
+    # Note that the 'user' property is currently commented out.
+    # If we set that property, it appears that the 'exec':
+    #
+    # a) must be run as root; and
+    # b) is then given root's environment, not that of the designated user.
+    # (That may not always be what we intend.)
+    #
+    # When the 'user' property is set and this 'exec' is then run as
+    # a non-root user, the following error occurs:
+    # "Error: Parameter user failed on Exec[...]:
+    # Only root can execute commands as other users""
+    
+    exec { 'Check values of environment variables':
+        command   => 'env',
+        path      => $exec_paths,
+        logoutput => 'true',
+        environment => [ $env_vars ]
+        # user      => 'cspace',
     }
     
     exec { 'Maven clean of Services layer source':
-        require => Vcsrepo[ '/tmp/cspace-source/services' ],
-        command => 'mvn clean',
+        command => $mvn_clean_cmd,
         cwd     => '/tmp/cspace-source/services',
-        path    => [ '/bin', '/usr/bin' ],
+        path    => $exec_paths,
+        require => Vcsrepo[ '/tmp/cspace-source/services' ],
     }
 
     # Download the UI layer
 
     vcsrepo { '/tmp/cspace-source/ui':
-        require  => File[ 'Create CollectionSpace source directory' ],
-        # require  => [ Package["git"] ],
         ensure   => latest,
         provider => 'git',
         source   => 'https://github.com/collectionspace/ui.git',
         revision => 'master',
+        require  => File[ 'Create CollectionSpace source directory' ],
     }
 
 }
@@ -123,9 +123,15 @@ class cspace_source( $env_vars ) {
 # Create an instance of this class
 
 class { 'cspace_source': 
-    env_vars => [ 
-                  'BAR=bar',
-				  'DB_PASSWORD_CSPACE=foobar',
-	              'FOO=foo',
-				]
+    env_vars   => [ 
+        'BAR=bar',
+        'DB_PASSWORD_CSPACE=foobar',
+        'FOO=foo',
+    ],
+    exec_paths => [
+        '/bin',
+        '/usr/bin',
+		'/usr/local/bin',
+    ],
+                
 }
