91ef832654840a8c20edddf47dab2bdea3bc3a60
Aron Roberts <aron@socrates.berkeley.edu>
2014-01-02 18:02:26 -0800

    CSPACE-6278: Added tags for selective building of layers. Set up more deterministic ordering of notifications and other resources.

--- manifests/init.pp
+++ manifests/init.pp
@@ -55,17 +55,30 @@ class cspace_source( $env_vars, $exec_paths = [ '/bin', '/usr/bin' ], $source_di
   
   # FIXME: Replace or augment with cross-platform compatible
   # methods for finding executables, including on Windows.
+    
+  notify{ 'Checking for build tools':
+    message => 'Checking for availability of Ant and Maven build tools ...',
+    tag     => [ 'services', 'application', 'ui' ],
+    before  => [
+        Exec [ 'Find Ant executable' ],
+        Exec [ 'Find Maven executable' ],
+    ],
+  }
   
   exec { 'Find Ant executable':
     command   => '/bin/sh -c "command -v ant"',
     path      => $exec_paths,
     logoutput => true,
+    tag       => [ 'services', 'application', 'ui' ],
+    before => Notify[ 'Creating source directory' ],
   }
   
   exec { 'Find Maven executable':
     command   => '/bin/sh -c "command -v mvn"',
     path      => $exec_paths,
     logoutput => true,
+    tag       => [ 'services', 'application', 'ui' ],
+    before => Notify[ 'Creating source directory' ],
   }
   
   # Note: The 'vcsrepo' resource, starting with version 0.2.0 of 2013-11-13,
@@ -91,13 +104,16 @@ class cspace_source( $env_vars, $exec_paths = [ '/bin', '/usr/bin' ], $source_di
     $cspace_source_dir = $default_cspace_source_dir
   }
 
+  notify{ 'Creating source directory':
+    message => 'Creating directory to hold CollectionSpace source code, if not present ...',
+    tag     => [ 'services', 'application', 'ui' ],
+    before  => File [ 'Ensure CollectionSpace source directory' ],
+  }
+  
   file { 'Ensure CollectionSpace source directory':
     ensure  => 'directory',
     path    => $cspace_source_dir,
-    require => [
-      Exec[ 'Find Ant executable' ],
-      Exec[ 'Find Maven executable' ],
-    ],
+    tag     => [ 'services', 'application', 'ui' ],
   }
   
   # ---------------------------------------------------------
@@ -106,16 +122,35 @@ class cspace_source( $env_vars, $exec_paths = [ '/bin', '/usr/bin' ], $source_di
   
   # Download the Application layer source code
   
+  # The Services layer build is dependent on the Application
+  # layer build, so Application layer source code is downloaded
+  # even when this manifest is invoked with the 'services' tag. 
+    
+  notify{ 'Downloading Application layer':
+    message => 'Downloading Application layer source code ...',
+    tag     => [ 'services', 'application' ],
+    before  => Vcsrepo [ 'Download Application layer source code' ],
+    require => File [ 'Ensure CollectionSpace source directory' ],
+  }
+  
   vcsrepo { 'Download Application layer source code':
     ensure   => latest,
     provider => 'git',
     source   => 'https://github.com/collectionspace/application.git',
     revision => 'master',
     path     => "${cspace_source_dir}/application",
+    tag      => [ 'services', 'application' ],
     require  => File[ 'Ensure CollectionSpace source directory' ],
   }
 
   # Download the Services layer source code
+  
+  notify{ 'Downloading Services layer':
+    message => 'Downloading Services layer source code ...',
+    tag     => 'services',
+    before  => Vcsrepo [ 'Download Services layer source code' ],
+    require  => File [ 'Ensure CollectionSpace source directory' ],
+  }
   
   vcsrepo { 'Download Services layer source code':
     ensure   => latest,
@@ -123,17 +158,26 @@ class cspace_source( $env_vars, $exec_paths = [ '/bin', '/usr/bin' ], $source_di
     source   => 'https://github.com/collectionspace/services.git',
     revision => 'master',
     path     => "${cspace_source_dir}/services",
-    require  => File[ 'Ensure CollectionSpace source directory' ],
+    tag      => 'services',
+    require  => File [ 'Ensure CollectionSpace source directory' ],
   }
   
   # Download the UI layer source code
 
+  notify{ 'Downloading UI layer':
+    message => 'Downloading UI layer source code ...',
+    tag     => 'ui',
+    before  => Vcsrepo [ 'Download UI layer source code' ],
+    require => File [ 'Ensure CollectionSpace source directory' ],
+  }
+  
   vcsrepo { 'Download UI layer source code':
     ensure   => latest,
     provider => 'git',
     source   => 'https://github.com/collectionspace/ui.git',
     revision => 'master',
     path     => "${cspace_source_dir}/ui",
+    tag      => 'ui',
     require  => File[ 'Ensure CollectionSpace source directory' ],
   }
   
@@ -145,12 +189,24 @@ class cspace_source( $env_vars, $exec_paths = [ '/bin', '/usr/bin' ], $source_di
   $mvn_clean_install_cmd = "${mvn_clean_cmd} install -DskipTests"
   
   # Build and deploy the Application layer
+
+  # The Services layer build is dependent on the Application
+  # layer build, so the Application layer build is performed
+  # even when this manifest is invoked with the 'services' tag. 
+    
+  notify{ 'Building Application layer':
+    message => 'Building and deploying Application layer ...',
+    tag     => [ 'services', 'application' ],
+    before  => Exec [ 'Build and deploy of Application layer source' ],
+    require => Vcsrepo[ 'Download Application layer source code' ],
+  }
   
   exec { 'Build and deploy of Application layer source':
     command     => $mvn_clean_install_cmd,
     cwd         => "${cspace_source_dir}/application",
     path        => $exec_paths,
     environment => $env_vars,
+    tag         => [ 'services', 'application' ],
     require     => [
       Vcsrepo[ 'Download Application layer source code' ],
       Exec[ 'Find Maven executable' ],
@@ -159,6 +215,13 @@ class cspace_source( $env_vars, $exec_paths = [ '/bin', '/usr/bin' ], $source_di
 
   # Build and deploy the Services layer
   
+  notify{ 'Building Services layer':
+    message => 'Building Services layer ...',
+    tag     => 'services',
+    before  => Exec [ 'Build of Services layer source' ],
+    require => Vcsrepo[ 'Download Services layer source code' ],
+  }
+  
   exec { 'Build of Services layer source':
     # Command below is a temporary placeholder during development
     # for the full build (very time consuming)
@@ -166,10 +229,17 @@ class cspace_source( $env_vars, $exec_paths = [ '/bin', '/usr/bin' ], $source_di
     cwd         => "${cspace_source_dir}/services",
     path        => $exec_paths,
     environment => $env_vars,
+    tag         => 'services',
     require     => [
       Vcsrepo[ 'Download Services layer source code' ],
       Exec[ 'Find Maven executable' ],
     ],
+  }
+  
+  notify{ 'Deploying Services layer':
+    message => 'Deploying Services layer ...',
+    tag     => 'services',
+    before  => Exec [ 'Deploy of Services layer source' ],
   }
   
   exec { 'Deploy of Services layer source':
@@ -179,6 +249,7 @@ class cspace_source( $env_vars, $exec_paths = [ '/bin', '/usr/bin' ], $source_di
     cwd         => "${cspace_source_dir}/services/services/JaxRsServiceProvider",
     path        => $exec_paths,
     environment => $env_vars,
+    tag         => 'services',
     require     => [
       Exec[ 'Build and deploy of Application layer source' ],
       Exec[ 'Build of Services layer source' ],
