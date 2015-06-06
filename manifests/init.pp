e066d096e59c178e83237064bdb9795b2b85e5f4
Aron Roberts <aron@socrates.berkeley.edu>
2015-06-05 17:30:12 -0700

    CSPACE-6629: Use least-privileges database user.

--- manifests/init.pp
+++ manifests/init.pp
@@ -52,7 +52,7 @@ include stdlib # for 'validate_array()'
 # global below, which is used as the default value for the 'source_code_revision' parameter.
 
 class cspace_source(
-  $db_cspace_user  = $cspace_postgresql_server::db_csadmin_user,
+  $db_cspace_user  = $cspace_postgresql_server::db_cspace_user,
   $env_vars        = $cspace_user::env::cspace_env_vars,
   $exec_paths      = [ '/bin', '/usr/bin' ],
   $source_code_rev = '',
